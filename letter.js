abc3e9650322bc59d403608003183d36e3cd7319
Stefanyds85 <beautyxyzu4@aol.com>
2018-01-19 03:35:42 -0500

    start

---
+++ letter.js
@@ -0,0 +1,13 @@
+function LetterOptions(letters) {
+    
+    const {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z} = letters;
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    this.e = e;
+    this.f = f;
+    this.g = g;
+};
+module.exports = LetterOptions;
---
+++ word.js
@@ -0,0 +1,13 @@
+function Words(wordList) {
+    
+    const { coding, profession, bootcamp, boolean, python, successful, stringify} = wordList;
+
+    this.coding = coding;
+    this.profession = profession;
+    this.bootcamp = bootcamp;
+    this.boolean = boolean;
+    this.python = python;
+    this.successful = successful;
+    this.stringify = stringify;
+};
+module.exports = Words;
